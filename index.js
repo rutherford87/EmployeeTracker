df8c6217ad1c49432db73d13f363b382701ce43b
Tyler Rutherford <rutherford87@gmail.com>
2021-04-26 22:12:58 -0500

    included start, restart, and add employee functions. Troubleshooting missing node module to test.

--- index.js
+++ index.js
@@ -2,6 +2,7 @@
 const mysql = require('mysql');
 const inquirer = require('inquirer');
 const cTable = require('console.table');
+const { type } = require('node:os');
 //npm start for package
 
 //Connections:
@@ -13,12 +14,103 @@ const connection = mysql.createConnection({
     user: 'root',
   
     // Your password
-    password: '',
+    password: '1tdwh2SQL!@',
     database: 'tracker_DB',
   });
 
 
   //start function 
+  const start = () => {
+    inquirer
+      .prompt({
+        name: 'viewOrAdd',
+        type: 'list',
+        message: 'Would you like to [ADD] an employee or [VIEW] employees?',
+        choices: ['ADD', 'VIEW', 'EXIT'],
+      })
+      .then((answer) => {
+        // based on their answer, either call the add or the view functions
+        if (answer.viewOrAdd === 'ADD') {
+          addEmp();
+        } else if (answer.postOrBid === 'VIEW') {
+          viewEmp();
+        } else {
+          connection.end();
+        }
+      });
+  };
+
   //restart function includes nested add employee, view employee, or connection.end();
+  const reStart = () => {
+    inquirer
+      .prompt({
+        name: 'restart',
+        type: 'list',
+        message: 'Would you like to [ADD] another employee, [VIEW] employees, or [EXIT]?',
+        choices: ['ADD', 'VIEW', 'EXIT'],
+      })
+      .then((answer) => {
+        // based on their answer, either call the add or the view functions
+        if (answer.restart === 'ADD') {
+          addEmp();
+        } else if (answer.postOrBid === 'VIEW') {
+          viewEmp();
+        } else {
+          connection.end();
+        }
+      });
+  };
+
   //function for add employees
+  const addEmp = () => {
+inquirer
+    .prompt ([
+{
+    name: 'empFirst',
+    type: 'input',
+    message: 'What is the employees first name?'
+},
+{
+    name: 'empLast',
+    type: 'input',
+    message: 'What is the employees last name last'
+},
+{
+    name: 'roleID',
+    type: 'input',
+    message: 'What is the employees role?'
+},{
+    name: 'managerID',
+    type: 'input',
+    message: 'Who is the employees manager?'
+}
+    ])
+.then((answers) => {
+    connection.query(
+        'INSERT INTO employees SET ?',
+        {
+            id: NULL,
+            first_name: answers.empFirst,
+            last_name: answers.empLast,
+            role_id: answers.roleID,
+            maanger_id: answers.maangerID
+        },
+        (err) => {
+            if (err) throw err;
+            console.log('Employee was added successfully!');
+        reStart();
+        }
+    )
+}
+)
+  }
   //function for viewing employees
+
+
+  //Initialize start function
+  connection.connect((err) => {
+    if (err) throw err;
+    // run the start function after the connection is made to prompt the user
+    start();
+  });
+  
